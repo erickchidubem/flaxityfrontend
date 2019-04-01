bcac02a0cdc14c2eb11ae6f5c95df63fbdfc233b
erickchidubem <erickchidubem@gmail.com>
2019-04-01 10:15:24 +0100

    machine-info-startt

--- src/app/logged-in/maintenance/machine-info/machine-info.component.ts
+++ src/app/logged-in/maintenance/machine-info/machine-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Utils } from 'src/app/shared/shared-service/utils';
+import { ContextService } from 'src/app/shared/shared-service/context.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-machine-info',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MachineInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(public utils : Utils,private context : ContextService,
+    private route: ActivatedRoute) { }
 
+  leadInfo : any=[];
+  id : any = this.route.params['value'].id;
+
+  tbPercentage ="30%";
   ngOnInit() {
+    this.getLeadInformation(this.id);
+  }
+
+  getLeadInformation(id){
+    this.utils.StartSpinner();
+      this.context.getWithToken(id,'account/viewleadinfo/').
+      subscribe( data => {
+        let d = <any>data;
+        this.leadInfo = d.data;
+        console.log(d.data)
+        this.utils.StopSpinner();
+      }); 
   }
 
 }
